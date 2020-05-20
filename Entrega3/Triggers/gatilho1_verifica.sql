a776761390d7328f4eb0920467ff1acc697516d7
Marina Dias <marinadias1500@hotmail.com>
2020-05-20 14:20:41 +0100

    Create gatilho1_verifica.sql

---
+++ Entrega3/Triggers/gatilho1_verifica.sql
@@ -0,0 +1,13 @@
+.mode       columns
+.headers    on
+.nullvalue  NULL
+
+select * from TipoDeBebida;
+select * from QuantidadeDeBebidas;
+select * from Cartao;
+
+INSERT INTO QuantidadeDeBebidas(id_cartao, id_servico, quantidade) VALUES ('5580', '4055', '3');
+
+select * from TipoDeBebida;
+select * from QuantidadeDeBebidas;
+select * from Cartao;
