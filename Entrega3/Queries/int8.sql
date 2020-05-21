3693aaf77b135c58ca700a18822d0121f2847440
Marina Dias <marinadias1500@hotmail.com>
2020-05-21 14:28:25 +0100

    Delete int8.sql

--- Entrega3/Queries/int8.sql
+++
@@ -1,4 +0,0 @@
---Pessoas que usaram os pontos do cartao para aplicar desconto e que ainda tem pontos no cartao
-
-select nome from Pessoa natural Join 
-(select nif from Cartao C natural Join DescontoAplicado D where D.desconto>0 and C.nr_pontos>0);
