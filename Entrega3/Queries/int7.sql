7b192d752eaca94d158179f89d29414c5bdd5b53
Marina Dias <marinadias1500@hotmail.com>
2020-05-24 16:44:32 +0100

    Update int7.sql

--- Entrega3/Queries/int7.sql
+++ Entrega3/Queries/int7.sql
@@ -4,5 +4,5 @@
 .headers    on
 .nullvalue  NULL
 
-select preco from PrecoDeQuarto join Quarto on Quarto.nr_quarto=PrecoDeQuarto.nr_quarto where (Quarto.tipo_quarto= ‘casal’ and percentagem>75 and data_inicio='2020-03-20' and data_fim='2020-06-21'); 
+select * from PrecoDeQuarto join Quarto on Quarto.nr_quarto=PrecoDeQuarto.nr_quarto where (Quarto.tipo_quarto= ‘casal’ and percentagem>75 and data_inicio='2020-03-20' and data_fim='2020-06-21'); 
 
