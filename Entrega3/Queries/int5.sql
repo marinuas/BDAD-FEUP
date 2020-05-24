94d67d4dd855dd9e63046dca89265f5cc5846492
Marina Dias <marinadias1500@hotmail.com>
2020-05-24 18:20:11 +0100

    okeyyy

--- Entrega3/Queries/int5.sql
+++ Entrega3/Queries/int5.sql
@@ -4,8 +4,16 @@
 .headers    on
 .nullvalue  NULL
 
-select solteiro.avgnr_pontos-familiar.avgnr_pontos as diferenca_de_pontos from
-(select avg(nr_pontos) as avgnr_pontos from Cartao natural Join Reserva where nr_quarto in
-(select nr_quarto from  Quarto where tipo_quarto="solteiro"))  as solteiro,
-(select avg(nr_pontos) as avgnr_pontos from Cartao natural Join Reserva where nr_quarto in (select nr_quarto
-from Quarto where tipo_quarto="familiar")) as familiar;
+drop view if exists Solteiro;
+create view Solteiro as
+select avg(nr_pontos) as avgnr_pontos from Cartao, Reserva, Quarto where (Reserva.nif=Cartao.nif and Quarto.nr_quarto=Reserva.nr_quarto 
+and Quarto.tipo_quarto="solteiro");
+
+
+drop view if exists Familiar;
+create view Familiar as
+select avg(nr_pontos) as avgnr_pontos from Cartao, Reserva, Quarto where (Reserva.nif=Cartao.nif and Quarto.nr_quarto=Reserva.nr_quarto 
+and Quarto.tipo_quarto="familiar");
+
+
+select Solteiro.avgnr_pontos-Familiar.avgnr_pontos as diferenca_media_de_pontos from Solteiro, Familiar;
