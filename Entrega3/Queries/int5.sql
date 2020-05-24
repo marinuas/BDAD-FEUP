1952ef8762e4cc8c54bf817fc86649cabd702cb4
Marina Dias <marinadias1500@hotmail.com>
2020-05-24 16:47:09 +0100

    Update int5.sql

--- Entrega3/Queries/int5.sql
+++ Entrega3/Queries/int5.sql
@@ -4,7 +4,7 @@
 .headers    on
 .nullvalue  NULL
 
-select solteiro.avgnr_pontos-familiar.avgnr_pontos from
+select solteiro.avgnr_pontos-familiar.avgnr_pontos as diferenca_de_pontos from
 (select avg(nr_pontos) as avgnr_pontos from Cartao natural Join Reserva where nr_quarto in
 (select nr_quarto from  Quarto where tipo_quarto="solteiro"))  as solteiro,
 (select avg(nr_pontos) as avgnr_pontos from Cartao natural Join Reserva where nr_quarto in (select nr_quarto
